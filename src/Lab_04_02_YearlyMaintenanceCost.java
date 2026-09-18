798d187baa86b0839f88d7589cda25fdc35a99a8
ryanb <balan@mail.uc.edu>
2026-09-18 10:48:56 -0400

    Initial commit

---
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,15 @@
+public class Lab_04_02_YearlyMaintenanceCost {
+static void main(){
+    int springCost = 5000;
+    int summerCost = 4500;
+    int fallCost = 6700;
+    int winterCost = 8900;
+
+    int yearlyCost = springCost + summerCost + fallCost + winterCost;
+
+    IO.println("The yearly maintenance cost for all 4 seasons combined is: $" + yearlyCost);
+
+
+        }
+
+}
